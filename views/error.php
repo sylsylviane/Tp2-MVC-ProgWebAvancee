31b98d42ea24715b3c86a1dfe15e472029d91451
sylsylviane <syl.sylviane1@gmail.com>
2024-12-03 19:25:38 -0500

    Créer page d'erreur

---
+++ views/error.php
@@ -0,0 +1,9 @@
+{{include('layouts/header.php', {title: 'Registration'})}}
+
+<div class="container">
+        <h2>Erreur</h2>
+        <strong class="error">404 page non trouvé ! </strong>
+        <p>{{msg}}</p>
+    </div>
+
+{{include('layouts/footer.php')}}
