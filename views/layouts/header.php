cb3583f881f20f9c18ce8a3bcfa695a87422c2d5
sylsylviane <syl.sylviane1@gmail.com>
2024-12-03 19:20:18 -0500

    Créer l'entête et navigation

---
+++ views/layouts/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de bord</title>
+    <link rel="stylesheet" href="{{asset}}css/style.css">
+</head>
+
+<body>
+    <nav class="nav-top">
+        <a href="{{base}}/home">Cinéma Paradiso</a>
+        <div>
+            <form action="#">
+                <label aria-label="search">
+                    <input type="search" placeholder="Search" name="search">
+                </label>
+            </form>
+            <a href="#">Connexion</a>
+            <a href="#">Déconnexion</a>
+        </div> 
+
+    </nav>
+    <div id="container">
+        <nav class="side-nav">
+            <ul>
+                <li><a href="{{base}}/home">Tableau de bord</a></li>
+
+                <li class="menu-deroulant"><a href="#">Tables ▿</a>
+                    <ul>
+                        <li><a href="">Cinema</a></li>
+                        <li><a href="">Salles</a></li>
+                        <li><a href="{{base}}/film">Films</a></li>
+                        <li><a href="">Genres</a></li>
+                        <li><a href="">Acteurs</a></li>
+                        <li><a href="">Réalisateurs</a></li>
+                    </ul>
+                </li>               
+            </ul> 
+        <ul> 
+        <li><a href="">Profil</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Déconnexion</a></li>
+    </ul>
+        </nav>
+        <main>
