b69dd7b2dc2f23209ee64fd064794830f7c93a50
sylsylviane <syl.sylviane1@gmail.com>
2024-12-03 19:33:14 -0500

    Ajouter les routes pour le projet

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 use App\Routes\Route;
 
 Route::get('/home', 'HomeController@index');
+Route::get('/film', 'FilmController@index');
+Route::get('/film/show', 'FilmController@show');
+Route::get('/film/create', 'FilmController@create');
+Route::post('/film/create', 'FilmController@store');
+Route::get('/film/edit', 'FilmController@edit');
+Route::post('/film/delete', 'FilmController@delete');
 
 Route::dispatch();
 
