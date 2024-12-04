155b3741238ca921776ab096f0f085ba5177ed63
sylsylviane <syl.sylviane1@gmail.com>
2024-12-04 11:48:13 -0500

    Créer html pour afficher un film

---
+++ views/film/show.php
@@ -0,0 +1,23 @@
+{{include('layouts/header.php', {title: 'Films'})}}
+
+<div class="container">
+    <h1>Film</h1>
+    <p><strong>Titre: </strong>{{ film.titre }}</p>
+    <p><strong>Genre: </strong>
+        {% for genre in genres %}
+        {% if genre.id == film.Genre_id %}
+        {{genre.nom}}
+        {% endif %}
+        {% endfor %}
+    </p>
+    <p><strong>Synopsis: </strong>{{ film.synopsis }}</p>
+    <p><strong>Date de sortie: </strong>{{ film.date_sortie }}</p>
+    <p><strong>Durée: </strong>{{ film.duree }}</p>
+    <a href="{{ base }}/film/edit?id={{film.id}}" class="btn block">Edit</a>
+    <form action="{{ base }}/film/delete" method="post">
+        <input type="hidden" titre="id" value="{{ film.id }}">
+        <button type="submit" class="btn block red">Delete</button>
+    </form>
+</div>
+
+{{include('layouts/footer.php')}}
