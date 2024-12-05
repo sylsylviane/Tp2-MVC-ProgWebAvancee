e67f0a8355f53dd1879a2c4a4da6c50aed9097c7
sylsylviane <syl.sylviane1@gmail.com>
2024-12-05 15:37:42 -0500

    Modifier pour appliqer style css

--- views/film/index.php
+++ views/film/index.php
@@ -1,17 +1,20 @@
 {{include('layouts/header.php', {title: 'Films'})}}
 
-<h1>Table</h1>
+<header>
+    <h1>Table</h1>
+</header>
 <h2>Film</h2>
-<table>
+<table class="table">
+    <thead>
     <tr>
         <th>Titre</th>
         <th>Synopsis</th>
         <th>Date de sortie</th>
         <th>Durée</th>
         <th>Genre</th>
-        <!-- <th>Réalisateur(s)</th>
-        <th>Acteur(s)</th> -->
     </tr>
+    </thead>
+    <tbody>
     {% for film in films %}
     <tr>
         <td><a href="{{base}}/film/show?id={{film.id}}">{{film.titre}}</a></td>
@@ -27,8 +30,9 @@
         </td>
     </tr>
     {% endfor %}
+    </tbody>
 </table>
 <br><br>
-<a href="{{base}}/film/create" class="btn">New Film</a>
+<a href="{{base}}/film/create" class="btn">Créer un film</a>
 
 {{include('layouts/footer.php')}}
